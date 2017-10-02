1580dce42691e59e1b1e0be6b85e2a8628cc8c72
Jan Aagaard <jan@aagaard.net>
2017-10-02 09:52:06 +0200

    Convert to TypeScript

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App

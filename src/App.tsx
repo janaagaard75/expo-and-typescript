45b95c0c36953a6b782eb91c506ebdb2c4c229a5
Jan Aagaard <jan@aagaard.net>
2017-10-02 10:20:27 +0200

    Lint the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { StyleSheet } from 'react-native'
+import { Text } from 'react-native'
+import { View } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center'
+  }
+})

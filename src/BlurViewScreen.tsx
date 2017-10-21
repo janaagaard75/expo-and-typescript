a9729f5f485a90d70204c9af1b94290257c70cdb
Jan Aagaard <jan@aagaard.net>
2017-10-21 21:18:26 +0200

    Clean up

--- src/BlurViewScreen.tsx
+++ src/BlurViewScreen.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react'
 import { BlurView } from 'expo'
 import { Image } from 'react-native'
-import { StatusBar } from 'react-native'
 import { StyleSheet } from 'react-native'
 import { View } from 'react-native'
 
-const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png'
+const expoImageUrl = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png'
 
 export class BlurViewScreen extends React.Component {
   public render() {
     return (
       <View style={{ flex: 1 }}>
-        <Image style={{ width: 192, height: 192 }} source={{ uri }} />
-
-        <BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill}>
-          <Image style={{ width: 96, height: 96 }} source={{ uri }} />
+        <Image
+          style={{
+            height: 192,
+            width: 192,
+          }}
+          source={{ uri: expoImageUrl }}
+        />
+        <BlurView
+          intensity={50}
+          style={StyleSheet.absoluteFill}
+          tint="light"
+        >
+          <Image
+            style={{
+              height: 96,
+              width: 96,
+            }}
+            source={{ uri: expoImageUrl }}
+          />
         </BlurView>
-
-        <StatusBar hidden={true} />
       </View>
     )
   }

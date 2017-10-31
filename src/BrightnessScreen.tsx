d5a24aa9c733db10f96459646d1b63ee2cce661a
Jan Aagaard <jan@aagaard.net>
2017-10-31 20:28:11 +0100

    Add buttons to brightness screen

--- src/BrightnessScreen.tsx
+++ src/BrightnessScreen.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { Component } from 'react'
+import { Button } from 'react-native'
 import { Brightness } from 'expo'
 import { NavigationScreenProps } from 'react-navigation'
 import { Text } from 'react-native'
 import { View } from 'react-native'
 
 interface State {
-  brightness: number | undefined
+  normalBrightness: number | undefined
   systemBrightness: number | undefined
 }
 
@@ -15,7 +16,7 @@ export class BrightnessScreen extends Component<NavigationScreenProps<void>, Sta
     super(props, context)
 
     this.state = {
-      brightness: undefined,
+      normalBrightness: undefined,
       systemBrightness: undefined,
     }
 
@@ -27,7 +28,7 @@ export class BrightnessScreen extends Component<NavigationScreenProps<void>, Sta
   }
 
   public render() {
-    if (this.state.brightness === undefined) {
+    if (this.state.normalBrightness === undefined) {
       return (
         <Text>Getting screen brightness...</Text>
       )
@@ -41,15 +42,59 @@ export class BrightnessScreen extends Component<NavigationScreenProps<void>, Sta
           justifyContent: 'center',
         }}
       >
-        <Text>Brightness: {this.state.brightness}</Text>
+        <Text>Normal brightness: {this.state.normalBrightness}</Text>
         <Text>System brightness: {this.state.systemBrightness}</Text>
+        <View style={{ marginTop: 10 }}>
+          <Button
+            onPress={() => this.updateBrightness()}
+            title="Refresh"
+          />
+        </View>
+        <View style={{ marginTop: 10 }}>
+          <Button
+            onPress={() => this.setNormalBrightness(0)}
+            title="Normal 0%"
+          />
+          <Button
+            onPress={() => this.setNormalBrightness(0.5)}
+            title="Normal 50%"
+          />
+          <Button
+            onPress={() => this.setNormalBrightness(1)}
+            title="Normal 100%"
+          />
+        </View>
+        <View style={{ marginTop: 10 }}>
+          <Button
+            onPress={() => this.setSystemBrightness(0)}
+            title="System 0%"
+          />
+          <Button
+            onPress={() => this.setSystemBrightness(0.5)}
+            title="System 50%"
+          />
+          <Button
+            onPress={() => this.setSystemBrightness(1)}
+            title="System 100%"
+          />
+        </View>
       </View>
     )
   }
 
+  private async setNormalBrightness(value: number) {
+    await Brightness.setBrightnessAsync(value)
+    this.updateBrightness()
+  }
+
+  private async setSystemBrightness(value: number) {
+    await Brightness.setSystemBrightnessAsync(value)
+    this.updateBrightness()
+  }
+
   private async updateBrightness() {
     this.setState({
-      brightness: await Brightness.getBrightnessAsync(),
+      normalBrightness: await Brightness.getBrightnessAsync(),
       systemBrightness: await Brightness.getSystemBrightnessAsync(),
     })
   }

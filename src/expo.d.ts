76067c8ec9f58d01abf0b455bf2744261a484f56
Jan Aagaard <jan@aagaard.net>
2017-12-24 14:21:02 +0100

    Add Button, font and glyphMap to icons

--- src/expo.d.ts
+++ src/expo.d.ts
@@ -2061,15 +2061,21 @@ declare module '@expo/vector-icons' {
     name: 'acrobat' | 'amazon' | 'android' | 'angellist' | 'aol' | 'appnet' | 'appstore' | 'bitbucket' | 'bitcoin' | 'blogger' | 'buffer' | 'cal' | 'call' | 'cart' | 'chrome' | 'cloudapp' | 'creativecommons' | 'delicious' | 'digg' | 'disqus' | 'dribbble' | 'dropbox' | 'drupal' | 'dwolla' | 'email' | 'eventasaurus' | 'eventbrite' | 'eventful' | 'evernote' | 'facebook' | 'fivehundredpx' | 'flattr' | 'flickr' | 'forrst' | 'foursquare' | 'github' | 'gmail' | 'google' | 'googleplay' | 'googleplus' | 'gowalla' | 'grooveshark' | 'guest' | 'html5' | 'ie' | 'instagram' | 'instapaper' | 'intensedebate' | 'itunes' | 'klout' | 'lanyrd' | 'lastfm' | 'lego' | 'linkedin' | 'lkdto' | 'logmein' | 'macstore' | 'meetup' | 'myspace' | 'ninetyninedesigns' | 'openid' | 'opentable' | 'paypal' | 'persona' | 'pinboard' | 'pinterest' | 'plancast' | 'plurk' | 'pocket' | 'podcast' | 'posterous' | 'print' | 'quora' | 'reddit' | 'rss' | 'scribd' | 'skype' | 'smashing' | 'songkick' | 'soundcloud' | 'spotify' | 'stackoverflow' | 'statusnet' | 'steam' | 'stripe' | 'stumbleupon' | 'tumblr' | 'twitter' | 'viadeo' | 'vimeo' | 'vk' | 'weibo' | 'wikipedia' | 'windows' | 'wordpress' | 'xing' | 'yahoo' | 'ycombinator' | 'yelp' | 'youtube'
   }
 
-  class Entypo extends Component<EntypoProps> { }
-  class EvilIcons extends Component<EvilIconsProps> { }
-  class Feather extends Component<FeatherProps> { }
-  class FontAwesome extends Component<FontAwesomeProps> { }
-  class Foundation extends Component<FoundationProps> { }
-  class Ionicons extends Component<IoniconsProps> { }
-  class MaterialCommunityIcons extends Component<MaterialCommunityIconsProps> { }
-  class MaterialIcons extends Component<MaterialIconsProps> { }
-  class Octicons extends Component<OcticonsProps> { }
-  class SimpleLineIcons extends Component<SimpleLineIconsProps> { }
-  class Zocial extends Component<ZocialProps> { }
+  abstract class BaseIconsComponent<Props> extends Component<Props> {
+    static Button: any
+    static font: any
+    static glyphMap: any
+  }
+
+  class Entypo extends BaseIconsComponent<EntypoProps> { }
+  class EvilIcons extends BaseIconsComponent<EvilIconsProps> { }
+  class Feather extends BaseIconsComponent<FeatherProps> { }
+  class FontAwesome extends BaseIconsComponent<FontAwesomeProps> { }
+  class Foundation extends BaseIconsComponent<FoundationProps> { }
+  class Ionicons extends BaseIconsComponent<IoniconsProps> { }
+  class MaterialCommunityIcons extends BaseIconsComponent<MaterialCommunityIconsProps> { }
+  class MaterialIcons extends BaseIconsComponent<MaterialIconsProps> { }
+  class Octicons extends BaseIconsComponent<OcticonsProps> { }
+  class SimpleLineIcons extends BaseIconsComponent<SimpleLineIconsProps> { }
+  class Zocial extends BaseIconsComponent<ZocialProps> { }
 }

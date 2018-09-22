2d943fe8e84a6e0920354c0a44cb3865793dc538
Jan Aagaard <jan@aagaard.net>
2018-09-22 14:28:49 +0200

    Avoid passing inline functions as props

--- src/BrightnessScreen.tsx
+++ src/BrightnessScreen.tsx
@@ -54,37 +54,37 @@ export class BrightnessScreen extends Component<NavigationScreenProps, State> {
         </View>
         <View style={{ marginTop: 10 }}>
           <Button
-            onPress={() => this.setNormalBrightness(0)}
+            onPress={this.setNormalBrightness(0)}
             title="Normal 0%"
           />
           <Button
-            onPress={() => this.setNormalBrightness(0.05)}
+            onPress={this.setNormalBrightness(0.05)}
             title="Normal 5%"
           />
           <Button
-            onPress={() => this.setNormalBrightness(0.5)}
+            onPress={this.setNormalBrightness(0.5)}
             title="Normal 50%"
           />
           <Button
-            onPress={() => this.setNormalBrightness(1)}
+            onPress={this.setNormalBrightness(1)}
             title="Normal 100%"
           />
         </View>
         <View style={{ marginTop: 10 }}>
           <Button
-            onPress={() => this.setSystemBrightness(0)}
+            onPress={this.setSystemBrightness(0)}
             title="System 0%"
           />
           <Button
-            onPress={() => this.setSystemBrightness(0.05)}
+            onPress={this.setSystemBrightness(0.05)}
             title="System 5%"
           />
           <Button
-            onPress={() => this.setSystemBrightness(0.5)}
+            onPress={this.setSystemBrightness(0.5)}
             title="System 50%"
           />
           <Button
-            onPress={() => this.setSystemBrightness(1)}
+            onPress={this.setSystemBrightness(1)}
             title="System 100%"
           />
         </View>
@@ -92,14 +92,18 @@ export class BrightnessScreen extends Component<NavigationScreenProps, State> {
     )
   }
 
-  private async setNormalBrightness(value: number) {
-    await Brightness.setBrightnessAsync(value)
-    this.updateBrightness()
+  private setNormalBrightness(value: number) {
+    return async () => {
+      await Brightness.setBrightnessAsync(value)
+      this.updateBrightness()
+    }
   }
 
-  private async setSystemBrightness(value: number) {
-    await Brightness.setSystemBrightnessAsync(value)
-    this.updateBrightness()
+  private setSystemBrightness(value: number) {
+    return async () => {
+      await Brightness.setSystemBrightnessAsync(value)
+      this.updateBrightness()
+    }
   }
 
   private async updateBrightness() {

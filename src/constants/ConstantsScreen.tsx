716dfb5d27028a5e4612c46ca55c93a3463c4e77
Jan Aagaard <jan@aagaard.net>
2018-11-05 20:30:55 +0100

    Sort imports

--- src/constants/ConstantsScreen.tsx
+++ src/constants/ConstantsScreen.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Component } from 'react'
 import { Constants } from 'expo'
+import { NavigationScreenProps } from 'react-navigation'
 import { Text } from 'react-native'
 import { View } from 'react-native'
-import { NavigationScreenProps } from 'react-navigation'
 
 interface State {
   webViewUserAgent: string | undefined

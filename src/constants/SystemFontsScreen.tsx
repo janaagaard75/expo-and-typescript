627b23e818a30793528aefcffa085a5ec87958c1
Jan Aagaard <jan@aagaard.net>
2019-09-20 21:25:18 +0200

    Remove deprecated comment

--- src/constants/SystemFontsScreen.tsx
+++ src/constants/SystemFontsScreen.tsx
@@ -12,7 +12,6 @@ export class SystemFontsScreen extends Component {
   public render() {
     return (
       <ScrollView>
-        {/* Cast to a string array (Constants.systemFonts as Array<string>)... if using the native types provided by expo insted of @types/expo. See tsconfig.json. */}
         {Constants.systemFonts.map((systemFont, index) => (
           <Text key={index}>{systemFont}</Text>
         ))}

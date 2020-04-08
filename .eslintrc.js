65d4f27fde61da2cfd3ca75e6a6b0bf56d1617e0
Jan Aagaard <jan@aagaard.net>
2020-04-08 21:27:36 +0200

    Save in JavaScript format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
-{
-  "parser": "@typescript-eslint/parser",
-  "extends": [
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-  "rules": {
+  rules: {
     "@typescript-eslint/array-type": [
       "error",
       {
-        "default": "generic",
-        "readonly": "generic"
-      }
+        default: "generic",
+        readonly: "generic",
+      },
     ],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-var-requires": "off",
-    "react/no-unescaped-entities": ["error", { "forbid": [">", "}"] }]
-  }
-}
+    "react/no-unescaped-entities": ["error", { forbid: [">", "}"] }],
+  },
+};

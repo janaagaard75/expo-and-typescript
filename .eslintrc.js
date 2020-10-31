631c221e02f0f577dd5ca57b2eee4dd552cfd430
Jan Aagaard <jan@aagaard.net>
2020-10-31 21:51:07 +0100

    Include ESLint's recommended rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
+    "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",

3b33b4c7b0325ed297ff8df9c83bc4a95370a2bb
Jan Aagaard <jan@aagaard.net>
2020-11-18 21:46:13 +0100

    Put path a variable

--- src/demoScreens/AssetScreen.tsx
+++ src/demoScreens/AssetScreen.tsx
@@ -4,11 +4,12 @@ import { Image, Text, View } from "react-native";
 
 export const AssetScreen = () => {
   const [assetsLoaded, setAssetsLoaded] = useState(false);
+  const wtfsPerMinutePath = "../../assets/wtfs-per-minute.png";
 
   useEffect(() => {
     const loadAssets = async () => {
       // Preloading is only required for published apps. This app isn't published, so this code has not been tested.
-      await Asset.loadAsync(require("../../assets/wtfs-per-minute.png"));
+      await Asset.loadAsync(require(wtfsPerMinutePath));
       setAssetsLoaded(true);
     };
     loadAssets();
@@ -29,7 +30,7 @@ export const AssetScreen = () => {
     >
       <Image
         resizeMode="contain"
-        source={require("../../assets/wtfs-per-minute.png")}
+        source={require(wtfsPerMinutePath)}
         style={{
           alignSelf: "stretch",
           flex: 1,

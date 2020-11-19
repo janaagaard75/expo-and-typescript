b3af737e166591b8a9176faab3e4cc2da2005f92
Jan Aagaard <jaa@cvation.com>
2020-11-19 08:08:38 +0100

    Center the button

--- src/demoScreens/AppleAuthenticationScreen.tsx
+++ src/demoScreens/AppleAuthenticationScreen.tsx
@@ -1,28 +1,36 @@
 import * as AppleAuthentication from "expo-apple-authentication";
 import React, { FunctionComponent } from "react";
+import { View } from "react-native";
 
 export const AppleAuthenticationScreen: FunctionComponent = () => (
-  <AppleAuthentication.AppleAuthenticationButton
-    buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-    buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-    cornerRadius={5}
-    onPress={async () => {
-      try {
-        await AppleAuthentication.signInAsync({
-          requestedScopes: [
-            AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-            AppleAuthentication.AppleAuthenticationScope.EMAIL,
-          ],
-        });
-        // signed in
-      } catch (e) {
-        if (e.code === "ERR_CANCELED") {
-          // handle that the user canceled the sign-in flow
-        } else {
-          // handle other errors
-        }
-      }
+  <View
+    style={{
+      alignItems: "center",
+      flex: 1,
     }}
-    style={{ width: 200, height: 44 }}
-  />
+  >
+    <AppleAuthentication.AppleAuthenticationButton
+      buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+      buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+      cornerRadius={5}
+      onPress={async () => {
+        try {
+          await AppleAuthentication.signInAsync({
+            requestedScopes: [
+              AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+              AppleAuthentication.AppleAuthenticationScope.EMAIL,
+            ],
+          });
+          // signed in
+        } catch (e) {
+          if (e.code === "ERR_CANCELED") {
+            // handle that the user canceled the sign-in flow
+          } else {
+            // handle other errors
+          }
+        }
+      }}
+      style={{ height: 44, marginTop: 100, width: 200 }}
+    />
+  </View>
 );

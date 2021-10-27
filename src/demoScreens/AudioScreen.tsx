893792e3b120f75fe08747cc1d81383b31740807
Jan Aagaard <jan@aagaard.net>
2021-10-27 22:35:17 +0200

    Use functional style

--- src/demoScreens/AudioScreen.tsx
+++ src/demoScreens/AudioScreen.tsx
@@ -1,63 +1,51 @@
 import { Audio } from "expo-av";
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Linking, Text, View } from "react-native";
 
-interface Props {}
+export const AudioScreen = () => {
+  const [soundLoaded, setSoundLoaded] = useState(false);
 
-interface State {
-  soundLoaded: boolean;
-}
+  useEffect(() => {
+    playSound();
+  }, []);
 
-export class AudioScreen extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      soundLoaded: false,
-    };
-
-    this.playSound();
-  }
-
-  public render() {
-    if (!this.state.soundLoaded) {
-      return <Text>Loading sound...</Text>;
-    }
-
-    return (
-      <View
-        style={{
-          alignItems: "center",
-          flex: 1,
-          justifyContent: "center",
-        }}
-      >
-        <Text>
-          Music from{" "}
-          <Text
-            style={{ color: "blue" }}
-            onPress={() =>
-              Linking.openURL(
-                "https://www.bensound.com/royalty-free-music/track/the-jazz-piano"
-              )
-            }
-          >
-            www.bensound.com
-          </Text>
-          .
-        </Text>
-      </View>
-    );
-  }
-
-  private async playSound() {
+  const playSound = async () => {
     await Audio.Sound.createAsync(
       require("../../assets/bensound-thejazzpiano.mp3"),
-      { shouldPlay: true }
+      {
+        shouldPlay: true,
+      }
     );
 
-    this.setState({
-      soundLoaded: true,
-    });
+    setSoundLoaded(true);
+  };
+
+  if (!soundLoaded) {
+    return <Text>Loading sound...</Text>;
   }
-}
+
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <Text>
+        Music from{" "}
+        <Text
+          style={{ color: "blue" }}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.bensound.com/royalty-free-music/track/the-jazz-piano"
+            )
+          }
+        >
+          www.bensound.com
+        </Text>
+        .
+      </Text>
+    </View>
+  );
+};
